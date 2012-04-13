7b1014af5a9737745fd2ae0ef093cd778a6c7340
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2012-04-13 00:15:22 -0600

    repo_0 20120412

---
+++ holamundo.c
@@ -0,0 +1,14 @@
+
+/**
+ *	Primer programa creado en lenguaje c compilado en djgpp y geany.
+ * 	Para compilar un programa desde djgpp sin geany hace desde la 
+ *	linea de conmandos con el siguiente comando: 
+ *	 	gcc -c nombre.c -o nombre.exe
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hola mundo\n");
+    return 0;
+}
