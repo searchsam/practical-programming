33b454086e65282523ef00d00b34425557063ff4
Samuel José Gutiérrez Avilés <search.sama@gmail.com>
2012-04-13 00:17:56 -0600

    Update holamundo.c

--- holamundo.c
+++ holamundo.c
@@ -1,9 +1,14 @@
 
 /**
  *	Primer programa creado en lenguaje c compilado en djgpp y geany.
+ * 
  * 	Para compilar un programa desde djgpp sin geany hace desde la 
  *	linea de conmandos con el siguiente comando: 
  *	 	gcc -c nombre.c -o nombre.exe
+ *  y agregar la siguiente linea antes de return 0;
+ *      system("PAUSE");
+ *  si esto no funciona prueba a añadir:
+ *      getch();
  */
 
 #include <stdio.h>
