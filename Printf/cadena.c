2825863ceb7a907d244e806a3a32403bcd65c051
Samuel Jose Gutierrez Aviles <search.sama@gmail.com>
2012-04-15 00:26:30 -0600

    repo_0 20120415

---
+++ Printf/cadena.c
@@ -0,0 +1,21 @@
+
+/*
+ *	Programa creado en lenguaje c, compilado en djgpp y geany.
+ */
+ 
+ #include <stdio.h>	/*	
+					 *	Para utilizar la funcion printf en nuestros programas debemos incluir la directiva: 
+					 *		#include <stdio.h> 
+					 *	al principio de programa.
+					 */
+
+int main()
+{
+	printf( "Cadena" );	/*
+						 *	Lo que pongamos entre las comillas es lo que vamos a sacar por pantalla.
+						 */
+
+	return 0;
+}
+
+/** Codigo de search-sam Samuel J. Gutierrez Aviles */
---
+++ Printf/retornocarro.c
@@ -0,0 +1,25 @@
+
+/*
+ *	Programa creado en lenguaje c, compilado en djgpp y geany.
+ */
+ 
+ #include <stdio.h>
+
+int main()
+{
+	printf( "Cadena\n" );
+	printf( "Segunda" );	/*
+							 *	Para escribir en la pantalla se usa un cursor que no vemos.
+							 *	Si lo que queremos es sacar cada una en una linea deberemos usar "\n". 
+							 *	Es el indicador de retorno de carro. 
+							 *	Lo que hace es saltar el cursor de escritura a la linea siguiente:
+							 *		Cadena
+							 *		Segunda
+							 */
+	
+	
+
+	return 0;
+}
+
+/** Codigo de search-sam Samuel J. Gutierrez Aviles */
