9cba50c07738d75d01be5d1ec3c19aa6e8f58aef
Samuel José Gutiérrez Avilés <search.sama@gmail.com>
2012-04-15 00:57:40 -0600

    Update holamundo.c

--- holamundo.c
+++ holamundo.c
@@ -1,6 +1,30 @@
 
+/*  
+ *  Copyright © 2012 CLINUX (Comunidad Linux UNI)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hola mundo\n");
+    return 0;
+}
+
 /**
- *	Primer programa creado en lenguaje c compilado en djgpp y geany.
+ *    Primer programa creado en lenguaje c compilado en djgpp y geany.
  * 
  * 	Para compilar un programa desde djgpp sin geany hace desde la 
  *	linea de conmandos con el siguiente comando: 
@@ -10,10 +34,3 @@
  *  si esto no funciona prueba a añadir:
  *      getch();
  */
-
-#include <stdio.h>
-
-int main() {
-    printf("Hola mundo\n");
-    return 0;
-}
